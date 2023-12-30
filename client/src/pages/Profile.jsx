0c1bdd130325fa2abc187f19aa175e472a4b10ba
ceenobi <cobimbachu@gmail.com>
2023-12-30 17:58:18 +0100

    fix resend link

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -101,7 +101,7 @@ const Profile = () => {
                     </div>
                   </div>
                   <p className="mb-1">{user?.email}</p>
-                  <div className="mb-2 d-md-flex gap-2">
+                  <div className="mb-2 d-md-flex align-items-center gap-2">
                     {loggedInUser._id === user?._id && (
                       <>
                         <p className="fw-bold mb-0">Account verification:</p>
